bad34ba511c0b13dc0889cbf1cd533fa6a00bf94
devlourenco <guilherme.gsldev@gmail.com>
2025-12-23 17:42:15 -0300

    Modelagem do banco de dados, NinjaModel e MissoesModel concluidos, annotations feitas para relacionamento de tabelas

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,6 +1,8 @@
-package dev.java10x.CadastroDeNinjas;
-
+package dev.java10x.CadastroDeNinjas.Ninjas;
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
@@ -9,9 +11,18 @@ public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String email;
+
     private int idade;
+
+    @ManyToOne
+    @JoinColumn(name = "missoes_id")
+    private MissoesModel missoes;
+
+
 
     public NinjaModel() {
     }
